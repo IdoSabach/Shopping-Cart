50daa08ad9fc1e6f5da89b01f9eb32257ddb0162
Ido <Idosabach11@gmail.com>
2024-03-31 21:14:45 +0300

    add popup for add cart

--- src/store/CardStore.jsx
+++ src/store/CardStore.jsx
@@ -2,17 +2,17 @@ import { create } from "zustand";
 
 const useCartStore = create((set) => ({
   cart: [],
-  addToCart: (product) =>
+  addToCart: (product, quantity=1) =>
     set((state) => {
       const existingIndex = state.cart.findIndex(
         (item) => item.id === product.id
       );
       if (existingIndex !== -1) {
         const updatedCart = [...state.cart];
-        updatedCart[existingIndex].quantity += 1;
+        updatedCart[existingIndex].quantity += quantity;
         return { cart: updatedCart };
       } else {
-        return { cart: [...state.cart, { ...product, quantity: 1 }] };
+        return { cart: [...state.cart, { ...product, quantity }] };
       }
     }),
   removeFromCart: (productId) =>
--- src/views/ProductPage/ProductPage.jsx
+++ src/views/ProductPage/ProductPage.jsx
@@ -1,59 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Items from "../../data/Items";
 import useCartStore from "../../store/CardStore.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const ProductPage = () => {
   const { id } = useParams();
   const product = Items.find((item) => item.id === parseInt(id));
 
+  const [quantity, setQuantity] = useState(1);
+  const [isClick, setIsClick] = useState(false);
   const { addToCart } = useCartStore();
+
+  const notify = () =>
+    toast.success(`${product.title} Added to your cart`, {
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      // transition: Bounce,
+    });
 
   if (!product) {
     return <div>Product not found</div>;
   }
 
   const handleAddToCart = () => {
-    addToCart(product);
+    addToCart(product, quantity); // Pass the selected quantity to addToCart
+    setIsClick(true);
+    notify();
   };
 
   return (
-    <div className="product-page flex flex-col lg:flex-row justify-center mt-8 gap-2 lg:gap-8 pt-2 pb-2">
-      <div className="product-image flex justify-center">
-        <img
-          src={product.image}
-          alt={product.title}
-          className="rounded-xl h-96 w-96 object-cover"
-        />
-      </div>
-      <div className="product-details flex flex-col justify-around p-4 lg:p-0 gap-6 lg:gap-0">
-
-        <div className="flex flex-col gap-2">
-        <h1 className="text-4xl font-bold">{product.title}</h1>
-        <p className="text-xl">{product.type}</p>
+    <div>
+      <div className="product-page flex flex-col lg:flex-row justify-center mt-8 gap-2 lg:gap-8 pt-2 pb-2">
+        <div className="product-image flex justify-center">
+          <img
+            src={product.image}
+            alt={product.title}
+            className="rounded-xl h-96 w-96 object-cover"
+          />
         </div>
-       
+        <div className="product-details flex flex-col justify-around p-4 lg:p-0 gap-6 lg:gap-0">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-bold">{product.title}</h1>
+            <p className="text-xl">{product.type}</p>
+          </div>
 
-        
-        <ul className="flex flex-col gap-2">
-        <p className="font-bold text-2xl">Description: </p>
-        <div className="text-xl">{product.description}</div>
-          {product.features.map((feature, index) => (
-            <li key={index} className="text-xl">{feature}</li>
-          ))}
-        </ul>
+          <ul className="flex flex-col gap-2">
+            <p className="font-bold text-2xl">Description: </p>
+            <div className="text-xl">{product.description}</div>
+            {product.features.map((feature, index) => (
+              <li key={index} className="text-xl">
+                {feature}
+              </li>
+            ))}
+          </ul>
 
-        <div className="flex justify-between items-center">
-          <p className="text-3xl font-bold">${product.price}</p>
-          <button
-            className="btn text-white bg-black text-2xl p-2 rounded-xl active:scale-95"
-            onClick={handleAddToCart}
-          >
-            Add to Cart
-          </button>
+          <div className="flex justify-between items-center">
+            <p className="text-3xl font-bold">${product.price}</p>
+            <select
+              name=""
+              id=""
+              className="text-2xl border"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+            >
+              {numArr.map((num) => (
+                <option key={num} value={num}>
+                  {num}
+                </option>
+              ))}
+            </select>
+            <div>
+              {isClick ? (
+                <div className="btn text-white bg-green-500 text-2xl p-2 rounded-xl flex gap-2 items-center">
+                  <img
+                    src="/images/icons8-approved-24.png"
+                    alt="v"
+                    className="w-6 h-6 filter invert"
+                  />
+                  Added to Cart
+                </div>
+              ) : (
+                <button
+                  className="btn text-white bg-black text-2xl p-2 rounded-xl active:scale-95 animate-bounce"
+                  onClick={handleAddToCart}
+                >
+                  Add to Cart
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        // transition:Bounce
+      />
     </div>
   );
 };
