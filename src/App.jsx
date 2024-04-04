80410d488a994d0632e5356d99a1cc6f2342530f
Ido <Idosabach11@gmail.com>
2024-04-04 14:13:49 +0300

    fix scroll top

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar/NavBar";
 import AppContent from "./components/AppContent/AppContent";
 import Footer from "./components/Footer/Footer";
 import MenuModal from "./components/MenuModal/MenuModal";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const App = () => {
   const [isCardOpen, setIdCardOpen] = useState(false);
@@ -16,6 +17,7 @@ const App = () => {
   return (
     <Router>
       <MenuModal isOpen={isCardOpen} closeMenu={handleOpen} />
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col justify-between">
         <div>
           <NavBar isOpen={handleOpen} />
